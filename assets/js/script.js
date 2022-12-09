95ff7cac6ede8b7cd3fe36151b8aca944c5bb8aa
Alexandria Ellis <allstarcoding777@outlook.com>
2022-12-08 18:00:58 -0600

    character counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@ const translatedText = document.querySelector('#translated-text');
 const translateBtn = document.querySelector("#translate-button");
 const userInputArea = document.querySelector("#text-area");
 
+const log = console.log;
+
 // TODO: give translateThis the contents of whatever the user wants to translate
 let translateThis;
-
 // gets translated text string from data
 function getString(response) {
-    let dataResponse = response.data.translatedText;
+	let dataResponse = response.data.translatedText;
     console.log(dataResponse);
     return dataResponse;
 }
@@ -42,33 +43,57 @@ function getTranslation() {
 	})
 	.then(response => getString(response))
     .then(function (dataResponse) {
-        console.log(dataResponse);
+		console.log(dataResponse);
 		// create span elements for each word in the translated sentence
 		let translationArray = stringToArray(dataResponse);
 		translatedText.replaceChildren(); // empties out last translation
 		for (let i = 0; i < translationArray.length; i++) {
 			let wordSpan = document.createElement('span');
 			// if the last word, don't add a space
-			if (translationArray[i] === translationArray.length-1) {
-				wordSpan.textContent = translationArray[i];
-			// otherwise add a space
-			} else {
-				wordSpan.textContent = translationArray[i] + " ";
+			const translationText = translationArray[i]
+			.toLocaleLowerCase()
+			.replace(
+				/['!','¡','?','¿','.',',',';',"'",'"',':',';','—','(',')','“','”',' ']|_/g,
+				""
+				)
+				if (translationArray[i] === translationArray.length-1) {
+					wordSpan.textContent = translationText;
+					cons
+					// otherwise add a space
+				} else {
+					wordSpan.textContent = translationText;
+				}
+				$(wordSpan).addClass("word");
+				translatedText.append(wordSpan);
 			}
-			wordSpan.addClass("word");
-			translatedText.append(wordSpan);
-		}
-    })
-	.catch(err => console.error(err));
-}
-//translate buttons event listener
-translateBtn.addEventListener("click", getTranslation)
+		})
+		.catch(err => console.error(err));
+	}
+	// displays character count limit
+	$('textarea').keyup(function() {
+		let characterCount = $(this).val().length,
+			current = $('#current'),
+			maximum = $('#max'),
+			theCount = $('#count');
+		current.text(characterCount);	
+	  });	  
+	//translate buttons event listener
+	translateBtn.addEventListener("click", getTranslation)
+	
+	// // clickable translation
+	// $(translatedText).on('click', function (event) {
+		// 	if ($(event.target).hasClass('word')) {
+// // 		// PUT THINGS YOU WANT TO HAPPEN AFTER CLICKING A WORD HERE
+
+// 	}
+
+// })
 
 // clickable translation
 $(translatedText).on('click', function (event) {
 	if ($(event.target).hasClass('word')) {
 		// PUT THINGS YOU WANT TO HAPPEN AFTER CLICKING A WORD HERE
 		console.log(event.target.textContent);
+		
 	}
-
 })
