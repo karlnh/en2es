8d1aeaafdd86123dcc0e0a3a76828d95475f6454
karlnh <hubigkarl@gmail.com>
2022-12-11 12:19:59 -0600

    add localstorage functionality; clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,30 @@ const wordModal = document.querySelector(".modal");
 let spanishWord = document.getElementById('spanishWord');
 let englishDefinition = document.getElementById('englishDefinition');
 let partOfSpeech = document.getElementById('partOfSpeech');
-let isOffensive = document.getElementById('isOffensive');
-
-// let word = document.querySelectorAll('.word')
-const log = console.log;
-
-// TODO: give translateThis the contents of whatever the user wants to translate
 let translateThis;
+
+// Initializing localStorage if empty
+let wordsJSON = JSON.parse(localStorage.getItem("words"));
+console.log(wordsJSON);
+if (wordsJSON === null) {
+	wordsJSON = [];
+	localStorage.setItem("words", JSON.stringify(wordsJSON));
+} else {
+	console.log("Loading words...")
+	for (let i = 0; i < wordsJSON.length; i++) {
+		console.log(wordsJSON[i]);
+	}
+}
+
+function setLocalStorage(string) {
+	const newWord = {
+		word: string
+	};
+	wordsJSON.push(newWord);
+	console.log(wordsJSON);
+	localStorage.setItem("words", JSON.stringify(wordsJSON));
+}
+
 // gets translated text string from data
 function getString(response) {
 	let dataResponse = response.data.translatedText;
@@ -23,7 +40,6 @@ function stringToArray(string) {
 	let arrayedTranslation = string.split(" ");
 	return arrayedTranslation;
 }
-
 // fetches API data and sends returned data into a text box
 function getTranslation() {
 	translateThis = userInputArea.value; // grabs user input from textbox and sets translateThis for the API
@@ -54,88 +70,74 @@ function getTranslation() {
 		let translationArray = stringToArray(dataResponse);
 		translatedText.replaceChildren(); // empties out last translation
 		for (let i = 0; i < translationArray.length; i++) {
+			console.log("Adding span...")
 			let wordSpan = document.createElement('span');
+			$(wordSpan).addClass('word');
 			// if the last word, don't add a space
-			const translationText = translationArray[i]
-			.toLocaleLowerCase()
-			.replace(
-				/['!','¡','?','¿','.',',',';',"'",'"',':',';','—','(',')','“','”',' ']|_/g,
-				""
-				)
-				if (translationArray[i] === translationArray.length-1) {
-					wordSpan.textContent = translationText;
-					// otherwise add a space
-				} else {
-					wordSpan.textContent = translationText;
-				}
-				$(wordSpan).addClass("word");
-				translatedText.append(wordSpan);
+			let translationText = translationArray[i];
+			if (translationArray[i] === translationArray.length - 1) {
+				wordSpan.textContent = translationText;
+			// otherwise add a space
+			} else {
+				wordSpan.textContent = translationText + " ";
 			}
-		})
-		.catch(err => console.error(err));
-	}
-	// displays character count limit
-	$('textarea').keyup(function() {
-		let characterCount = $(this).val().length,
-			current = $('#current'),
-			maximum = $('#max'),
-			theCount = $('#count');
-		current.text(characterCount);	
-	  });	  
-	//translate buttons event listener
-	translateBtn.addEventListener("click", getTranslation)
+			translatedText.append(wordSpan);
+		}
+	})
+	.catch(err => console.error(err));
+}
+// displays character count limit
+$('textarea').keyup(function() {
+	let characterCount = $(this).val().length,
+		current = $('#current'),
+		maximum = $('#max'),
+		theCount = $('#count');
+	current.text(characterCount);	
+	});	  
+//translate buttons event listener
+translateBtn.addEventListener("click", getTranslation)
 
 // clickable translation
 $(translatedText).on('click', function (event) {
 	if ($(event.target).hasClass('word')) {
+		let cleanedWord = event.target.textContent.toLocaleLowerCase().replace(/[!¡?¿,.;'":;—()“” ]|_/g,"");
+		console.log(cleanedWord);
 		// PUT THINGS YOU WANT TO HAPPEN AFTER CLICKING A WORD HERE
+		// local storage object
+		setLocalStorage(cleanedWord);
+		//make modal visible and invisible
 		$(wordModal).addClass('is-active');
-		fetch('https://dictionaryapi.com/api/v3/references/spanish/json/' + event.target.textContent + '?key=97335289-85ec-42ac-8ab6-d1b79ac4a8df')
+		$(wordModal).on('click', function (event) {
+			if ($(event.target).hasClass("delete")) {
+				$(wordModal).removeClass('is-active');
+			}	
+		});
+		fetch('https://dictionaryapi.com/api/v3/references/spanish/json/' + cleanedWord + '?key=97335289-85ec-42ac-8ab6-d1b79ac4a8df')
 		.then( response => {
-		  if (response.ok) {console.log('Sucessful Fetch')}
-		  else console.log('Fetch Failed')
-		  return response
+		console.log(response);
+		if (response.ok) {
+			console.log('Sucessful Fetch')
+		}
+		else {
+			console.log('Fetch Failed');
+		}
+		return response;
 		})
 		.then(response => response.json())
 		.then(data => handleData(data))
-		.catch(error => console.log('error')) 
+		.catch(err => console.error(err));
 	   
 		function handleData(data) {
 		 //Modal stuff goes here
 		 spanishWord.textContent = 'Spanish Word: ' + data[0].meta.id;
 		 englishDefinition.textContent = 'Definition in English: ' + data[0].shortdef;
 		 partOfSpeech.textContent = 'Part of Speech: ' + data[0].fl;
-		 offensiveWord.textContent = 'Is the word considered offensive: ' + data[0].meta.offensive;
 
 		  console.log(data);
 		  console.log('Spanish Word: ' + data[0].meta.id)
 		  console.log('Definition in English: ' + data[0].shortdef)
 		  console.log('Part of Speech: ' + data[0].fl)
-		  console.log('Is the word considered offensive: ' + data[0].meta.offensive)
-		
-	   
-		console.log(event.target.textContent);
-		loacalStr(event.target.textContent)
+		// getLocalStorage(event.target.textContent)
 	}
 }
 })
-
-$(wordModal).on('click', function (event) {
-	if ($(event.target).hasClass("delete")) {
-	$(wordModal).removeClass('is-active')
-}
-	})
-
-let saved = document.getElementById("translate-section");
-saved.addEventListener("click", loacalStr);
-
-
-function loacalStr(word){
-	console.log(word)
-
-	localStorage.setItem('key', word)
-	
-
-	// localStorage.getItem(key)
-	console.log(localStorage.getItem('key'))
-}
